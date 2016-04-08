aaca441170cb2ae6e08034f084685e454219465f
Tatu Saloranta <tatu.saloranta@iki.fi>
2016-04-07 18:23:11 -0700

    Add first version of full-block compressor

--- src/main/java/com/fasterxml/jackson/dataformat/spade/BitmapEncoder.java
+++ src/main/java/com/fasterxml/jackson/dataformat/spade/BitmapEncoder.java
@@ -2,10 +2,23 @@ package com.fasterxml.jackson.dataformat.spade;
 
 public class BitmapEncoder
 {
+    private final static byte ZERO_BYTE = 0;
+
+    /**
+     * Buffer from which input to encode is read.
+     */
     protected final byte[] _input;
 
     protected final byte[] _output;
 
+    int _inputPtr;
+    
+    // Pointer to point after last byte actually output
+    int _outputTail;
+
+    // 8-bit value that constitutes continuation of the match
+    int _matchLevel1 = 0x0; // starts with clear bits
+    
     public BitmapEncoder(byte[] input, int bitLength)
     {
         _input = input;
@@ -14,12 +27,241 @@ public class BitmapEncoder
         if (rem > 0) {
             _fixLast(input, (bitLength>>3), rem);
         }
-        // Should need at most 1/8 + 1/64 + 1/512 extra, round up to 3/16
+        // Max overhead at level 1 is 1/32; at level 2 it's 1/64; and at level 3 1/512.
+        // So let's round up to 1/16 for convenience
         int maxNeeded = input.length; 
-        maxNeeded +=  ((maxNeeded + 7) >> 3) + ((maxNeeded + 15) >> 4);
+        maxNeeded +=  ((maxNeeded + 15) >> 4) + 4;
         _output = new byte[maxNeeded];
     }
 
+    /**
+     * Top-level compressor that calls {@link #_encodeLevel2} to handle 8 of 512-byte chunks,
+     * resulting in 4k input blocks, with 8-bit mask to indicate which chunks contain
+     * literal bytes.
+     */
+    int _encodeLevel3(int outputPtr)
+    {
+        // Let's do this unrolled:
+        int resultMask = _encodeLevel2(outputPtr+1);
+        if (resultMask != 0) { // had output, so prepend mask
+            _output[outputPtr] = (byte) resultMask;
+            resultMask |= 0x80;
+            outputPtr = _outputTail;
+        }
+
+        // and then 7 more times
+        int mask = _encodeLevel2(outputPtr+1);
+        if (mask != 0) {
+            _output[outputPtr] = (byte) mask;
+            resultMask |= 0x40;
+            outputPtr = _outputTail;
+        }
+        mask = _encodeLevel2(outputPtr+1);
+        if (mask != 0) {
+            _output[outputPtr] = (byte) mask;
+            resultMask |= 0x20;
+            outputPtr = _outputTail;
+        }
+        mask = _encodeLevel2(outputPtr+1);
+        if (mask != 0) {
+            _output[outputPtr] = (byte) mask;
+            resultMask |= 0x10;
+            outputPtr = _outputTail;
+        }
+        mask = _encodeLevel2(outputPtr+1);
+        if (mask != 0) {
+            _output[outputPtr] = (byte) mask;
+            resultMask |= 0x08;
+            outputPtr = _outputTail;
+        }
+        mask = _encodeLevel2(outputPtr+1);
+        if (mask != 0) {
+            _output[outputPtr] = (byte) mask;
+            resultMask |= 0x04;
+            outputPtr = _outputTail;
+        }
+        mask = _encodeLevel2(outputPtr+1);
+        if (mask != 0) {
+            _output[outputPtr] = (byte) mask;
+            resultMask |= 0x02;
+            outputPtr = _outputTail;
+        }
+        mask = _encodeLevel2(outputPtr+1);
+        if (mask != 0) {
+            _output[outputPtr] = (byte) mask;
+            resultMask |= 0x01;
+            outputPtr = _outputTail;
+        }
+        return resultMask;
+    }
+    
+    /**
+     * Second-level encoding function that delegates to {@link #_encodeFullLevel1}
+     * for 32 byte chunks, calling it 8 x 2 times, for total block size of
+     * 512 bytes, with up to 16 marker bytes. Returns 8 bit mask to indicate chunks
+     * that are NOT empty.
+     *
+     * @return 8-bit mask of blocks produced
+     */
+    int _encodeLevel2(int outputPtr)
+    {
+        int resultMask = 0;
+
+        // Need 8 loops of 8 bytes each, with each level1-call getting a nibble
+        int rounds = 8;
+        // First: leave room for one-byte byte marker
+        while (true) {
+            final int origOutputPtr = outputPtr;
+            ++outputPtr;
+            int mask = _encodeFullLevel1(outputPtr);
+            if (mask != 0) { // not a full run, appended output
+                mask <<= 4;
+                outputPtr = _outputTail;
+            }
+            int lo = _encodeFullLevel1(outputPtr);
+            if (lo != 0) {
+                outputPtr = _outputTail;
+                mask |= lo;
+            }
+            if (mask == 0) { // no output, reset position
+                outputPtr = origOutputPtr;
+            } else { // had output, so prepend mask
+                _output[origOutputPtr] = (byte) mask;
+                resultMask |= 1;
+            }
+            if (--rounds == 0) {
+                break;
+            }
+            resultMask <<= 1;
+        }
+        return resultMask;
+    }
+    
+    /**
+     * Lowest-level encoding method for full blocks: handles 32 bytes, that is, 256 bits.
+     * Contains one special optimization for "non-compressing" content. Note that
+     * contents of return value will be written as prefix bytes, as necessary (zeroes
+     * omitted).
+     *
+     * @return 4-bit mask to indicate which of potential 8-byte blocks are included (that is,
+     *    have further presence bit and 1-8 bytes of underlying data).
+     */
+    int _encodeFullLevel1(int outputPtr)
+    {
+        int match = _matchLevel1;
+        int resultBits = 0;
+        int origOutputPtr = outputPtr; // to check whether compression achieved
+        int inputPtr = _inputPtr;
+
+        /*
+System.err.print(" encodeLevel1 #"+inputPtr+": ");        
+for (int i = 0; i < 32; ++i) {
+    System.err.printf(" %02x", _input[_inputPtr+i] & 0xFF);
+}
+System.err.println();
+*/
+        
+        // Need 4 loops of 8 bytes each as prefixes are interleaved
+        int rounds = 4;
+        while (true) {
+            final int baseOut = outputPtr;
+            int mask = 0; // lowest-level mask for group of 8 bytes
+
+            byte b = _input[inputPtr++];
+            // Basic component, repeated 8 times: see if run continues; if not, output byte, add bit
+            if ((b & 0xFF) != match) {
+                _output[++outputPtr] = b; // important: advance first, to leave room for prefix
+                match = ((b & 0x1) == 0) ? 0 : 0xFF;
+                mask |= 0x80;
+            }
+            // and then repeat 7 more times
+            b = _input[inputPtr++];
+            if ((b & 0xFF) != match) {
+                _output[++outputPtr] = b;
+                match = ((b & 0x1) == 0) ? 0 : 0xFF;
+                mask |= 0x40;
+            }
+            b = _input[inputPtr++];
+            if ((b & 0xFF) != match) {
+                _output[++outputPtr] = b;
+                match = ((b & 0x1) == 0) ? 0 : 0xFF;
+                mask |= 0x20;
+            }
+            b = _input[inputPtr++];
+            if ((b & 0xFF) != match) {
+                _output[++outputPtr] = b;
+                match = ((b & 0x1) == 0) ? 0 : 0xFF;
+                mask |= 0x10;
+            }
+            b = _input[inputPtr++];
+            if ((b & 0xFF) != match) {
+                _output[++outputPtr] = b;
+                match = ((b & 0x1) == 0) ? 0 : 0xFF;
+                mask |= 0x08;
+            }
+            b = _input[inputPtr++];
+            if ((b & 0xFF) != match) {
+                _output[++outputPtr] = b;
+                match = ((b & 0x1) == 0) ? 0 : 0xFF;
+                mask |= 0x04;
+            }
+            b = _input[inputPtr++];
+            if ((b & 0xFF) != match) {
+                _output[++outputPtr] = b;
+                match = ((b & 0x1) == 0) ? 0 : 0xFF;
+                mask |= 0x02;
+            }
+            b = _input[inputPtr++];
+            if ((b & 0xFF) != match) {
+                _output[++outputPtr] = b;
+                match = ((b & 0x1) == 0) ? 0 : 0xFF;
+                mask |= 0x01;
+            }
+
+            // and then assess the situation: did we output anything?
+            if (mask != 0) { // yup: add prefix mask
+                _output[baseOut] = (byte) mask;
+                ++outputPtr; // since it pointed to the last added byte
+                resultBits |= 1;
+            }
+            if (--rounds == 0) {
+                break;
+            }
+            resultBits <<= 1;
+        }
+
+        // Three main outcomes:
+        //
+        // (a) completely compressed out; nothing output, zero returned
+        // (b) enough compression, left as is
+        // (c) not enough compression; rewrite with single 0 byte, copy 32 literal bytes after
+        //    (which is safe as zero bytes are never otherwise encoded, "parent-bit" above indicates zero/non-zero)
+        
+        // and then update settings unless we had full run
+        if (resultBits != 0) { // (b) or (c)
+            int amount = outputPtr - origOutputPtr;
+
+            if (amount > 32) { // (c), need to re-do
+                _output[origOutputPtr++] = ZERO_BYTE;
+                System.arraycopy(_input, _inputPtr, _output, origOutputPtr, 32);
+                outputPtr = origOutputPtr+32;
+                // also ensure we declare everything to be non-compressed
+                resultBits = 0xF;
+            }
+            // at lowest level we are sure to advance the pointer
+            _outputTail = outputPtr;
+            _matchLevel1 = match;
+        }
+        _inputPtr = inputPtr;
+        return resultBits;
+    }
+
+    /*
+    /**********************************************************************
+    /* Internal helper methods
+    /**********************************************************************
+     */
+    
     // Helper method for changing extra unused bits to be the same
     // as the last actual content bit; this to make sure last run
     // is not accidentally broken by garbage
@@ -38,45 +280,5 @@ public class BitmapEncoder
         if (changed != old) {
             input[offset] = (byte) changed;
         }
-    }
-
-    int _inputPtr;
-    
-    // Pointer to point after last byte actually output
-    int _outputTail;
-
-    // 8-bit value that constitutes continuation of the match
-    int _matchLevel1 = 0x0; // starts with clear bits
-
-    // Lowest-level encoding method for full blocks: handles 32 bytes, that is, 256 bits
-    int _encodeFullLevel1(int outputPtr)
-    {
-        int match = _matchLevel1;
-        int result = 0;
-
-        for (int i = 0; i < 32; ++i) {
-            byte b = _input[_inputPtr++];
-            result <<= 1;
-            // If run continues, nothing to output
-            if ((b & 0xFF) == match) {
-                continue;
-            }
-            // but if run ended, need to output byte, indicate non-empty
-            _output[outputPtr++] = b;
-            if ((b & 0x1) == 0) { // empty last bit?
-                match = 0;
-            } else { // last bit was set
-                match = 0xFF;
-            }
-            result |= 1;
-        }
-
-        // and then update settings unless we had full run
-        if (result != 0) {
-            // at lowest level we are sure to advance the pointer
-            _outputTail = outputPtr;
-            _matchLevel1 = match;
-        }
-        return result;
     }
 }
--- src/test/java/com/fasterxml/jackson/dataformat/spade/BitmapEncoderTest.java
+++ src/test/java/com/fasterxml/jackson/dataformat/spade/BitmapEncoderTest.java
@@ -8,6 +8,8 @@ public class BitmapEncoderTest extends ModuleTestBase
     {
         // with 4 bits in last byte, last bit is 1, so should fill
         _verifyFixLast(0xF0, 4, 0xFF);
+        _verifyFixLast(0xF0, 5, 0xF0);
+        _verifyFixLast(0x80, 1, 0xFF);
     }
     
     private void _verifyFixLast(int base, int lastBits, int exp)
@@ -26,7 +28,7 @@ public class BitmapEncoderTest extends ModuleTestBase
         assertEquals(0, b[2]);
     }
 
-    public void testFullLevel1()
+    public void testLevel1Compressing()
     {
         byte[] input = new byte[32];
         BitmapEncoder encoder = new BitmapEncoder(input, input.length * 8);
@@ -48,31 +50,79 @@ public class BitmapEncoderTest extends ModuleTestBase
         Arrays.fill(input, (byte) 0xFF);
         encoder = new BitmapEncoder(input, input.length * 8);
         result = encoder._encodeFullLevel1(0);
-        assertEquals(0x80000000, result);
+        // 4-bit mask with first one set
+        assertEquals(0x8, result);
         assertEquals(32, encoder._inputPtr);
-        assertEquals(1, encoder._outputTail);
-        assertEquals(0xFF, encoder._output[0] & 0xFF);
+        assertEquals(2, encoder._outputTail); // 1 changed bit plus the prefix
+        assertEquals(0x80, encoder._output[0] & 0xFF);
+        assertEquals(0xFF, encoder._output[1] & 0xFF);
         assertEquals(0xFF, encoder._matchLevel1);
 
-        // and more, with 16 0s, 16s, yet different
+        // and more, with 16 0s, 16 1s, yet different
         input = new byte[32];
         Arrays.fill(input, 16, 32, (byte) 0xFF);
         encoder = new BitmapEncoder(input, input.length * 8);
         result = encoder._encodeFullLevel1(0);
-        assertEquals(0x8000, result);
+        assertEquals(0x02, result);
         assertEquals(32, encoder._inputPtr);
-        assertEquals(1, encoder._outputTail);
-        assertEquals(0xFF, encoder._output[0] & 0xFF);
+        assertEquals(2, encoder._outputTail); // 1 changed bit plus the prefix
+        assertEquals(0x80, encoder._output[0] & 0xFF);
+        assertEquals(0xFF, encoder._output[1] & 0xFF);
         assertEquals(0xFF, encoder._matchLevel1);
+    }
 
-        // and finally with zigzag pattern
+    // Test that verifies our optimization for not-well-enough compression
+    public void testLevel1NotEnoughCompressible()
+    {
+        byte[] input = new byte[32];
+        BitmapEncoder encoder = new BitmapEncoder(input, input.length * 8);
+
+        input = new byte[32];
+
+        // make first and last two compressible; still not enough (34 bytes, vs 33 with 'no-comp')
+        Arrays.fill(input, (byte) 0xAA);
+        input[0] = 0;
+        input[30] = input[31] = -1;
+        
+        encoder = new BitmapEncoder(input, input.length * 8);
+        int result = encoder._encodeFullLevel1(0);
+        assertEquals(0xF, result);
+        assertEquals(32, encoder._inputPtr);
+        // Without optimization would be 36, with optimization 33
+        assertEquals(33, encoder._outputTail);
+        // and the marker we have is 0x0, otherwise illegal as marker (that is,
+        // never produced by encoder)
+        assertEquals(0x00, encoder._output[0] & 0xFF);
+        assertEquals(0x00, encoder._output[1] & 0xFF); // first byte
+        for (int i = 2; i <= 30; ++i) {
+            assertEquals(0xAA, encoder._output[i] & 0xFF);
+        }
+        assertEquals(0xFF, encoder._output[31] & 0xFF); // second-to-last byte
+        assertEquals(0xFF, encoder._output[32] & 0xFF); // last byte
+    }
+
+    // And test for stuff that does not have any detectable runs (fully non-compressible)
+    public void testLevel1NonCompressible()
+    {
+        byte[] input = new byte[32];
+        BitmapEncoder encoder = new BitmapEncoder(input, input.length * 8);
+
+        // Test with zigzag pattern
         input = new byte[32];
         Arrays.fill(input, (byte) 0xAA);
         encoder = new BitmapEncoder(input, input.length * 8);
-        result = encoder._encodeFullLevel1(0);
-        assertEquals(-1, result);
+        int result = encoder._encodeFullLevel1(0);
+        assertEquals(0xF, result);
         assertEquals(32, encoder._inputPtr);
-        assertEquals(32, encoder._outputTail);
+        // Without optimization would be 36, with optimization 33
+        assertEquals(33, encoder._outputTail);
+        // and the marker we have is 0x0, otherwise illegal as marker (that is,
+        // never produced by encoder)
+        assertEquals(0x00, encoder._output[0] & 0xFF);
+        for (int i = 1; i <= 33; ++i) {
+            assertEquals(0xAA, encoder._output[i] & 0xFF);
+        }
+        
         // the very last bit is 0 so
         assertEquals(0x00, encoder._matchLevel1);
 
@@ -81,10 +131,175 @@ public class BitmapEncoderTest extends ModuleTestBase
         Arrays.fill(input, (byte) 0x55);
         encoder = new BitmapEncoder(input, input.length * 8);
         result = encoder._encodeFullLevel1(0);
-        assertEquals(-1, result);
+        assertEquals(0xF, result);
         assertEquals(32, encoder._inputPtr);
-        assertEquals(32, encoder._outputTail);
+        assertEquals(33, encoder._outputTail);
         // the very last bit is now 1:
         assertEquals(0xFF, encoder._matchLevel1);
+        assertEquals(0x00, encoder._output[0] & 0xFF);
+        for (int i = 1; i <= 33; ++i) {
+            assertEquals(0x55, encoder._output[i] & 0xFF);
+        }
+    }
+
+    public void testLevel2Compressing()
+    {
+        final int BYTES = 32 * 16;
+        byte[] input;
+        BitmapEncoder encoder;
+        int result;
+
+        // with empty contents should just get straight run
+        input = new byte[BYTES];
+        encoder = new BitmapEncoder(input, input.length * 8);
+        assertEquals(0, encoder._encodeLevel2(0));
+        // but has now consumed input...
+        assertEquals(BYTES, encoder._inputPtr);
+        assertEquals(0, encoder._outputTail);
+        assertEquals(0, encoder._matchLevel1);
+
+        // with all 1s, bit different due to assumption of starting with '0'
+        input = new byte[BYTES];
+        Arrays.fill(input, (byte) 0xFF);
+        encoder = new BitmapEncoder(input, input.length * 8);
+        // 8-bit mask with first one set
+        assertEquals(0x80, encoder._encodeLevel2(0));
+        assertEquals(BYTES, encoder._inputPtr);
+        assertEquals(3, encoder._outputTail); // 1 actual byte at low level, 2 levels of masks
+        assertEquals(0x80, encoder._output[0] & 0xFF);
+        assertEquals(0x80, encoder._output[1] & 0xFF);
+        assertEquals(0xFF, encoder._output[2] & 0xFF);
+        assertEquals(0xFF, encoder._matchLevel1);
+
+        // and more, with half 0s, followed by half 1s
+        input = new byte[BYTES];
+        Arrays.fill(input, BYTES/2, input.length, (byte) 0xFF);
+        encoder = new BitmapEncoder(input, input.length * 8);
+        // should not be completely empty...
+        result = encoder._encodeLevel2(0);
+        assertEquals(BYTES, encoder._inputPtr);
+        assertEquals(0x08, result);
+        assertEquals(3, encoder._outputTail); // 1 data byte, plus 2 levels of prefixes
+        assertEquals(0x80, encoder._output[0] & 0xFF);
+        assertEquals(0x80, encoder._output[1] & 0xFF);
+        assertEquals(0xFF, encoder._output[2] & 0xFF);
+        assertEquals(0xFF, encoder._matchLevel1);
+    }
+
+    public void testLevel2NotCompressing()
+    {
+        final int BYTES = 32 * 16;
+        byte[] input;
+        BitmapEncoder encoder;
+
+        // if non-compressing, should just get sets of literals
+        input = new byte[BYTES];
+        Arrays.fill(input, (byte)0xAA);
+        encoder = new BitmapEncoder(input, input.length * 8);
+        assertEquals(0xFF, encoder._encodeLevel2(0));
+        assertEquals(BYTES, encoder._inputPtr);
+        assertEquals(0, encoder._matchLevel1); // ends with 0-bit
+
+        assertEquals(BYTES, encoder._inputPtr);
+        // 1/32 for level 1 (optimized from 1/8); further 1/64 from level 2
+        assertEquals(BYTES + BYTES/32 + BYTES/64, encoder._outputTail);
+
+        int ptr = 0;
+
+        byte[] outBuf = encoder._output;
+        for (int level2 = 0; level2 < 8; ++level2) { // 8 sections on level 2
+            assertEquals(0xFF, outBuf[ptr++] & 0xFF); // mask bits full at level2
+            assertEquals(0, outBuf[ptr++] & 0xFF); // short-cut marker to indicate "all 32"
+            for (int i = 0; i < 32; ++i) {
+                assertEquals(0xAA, outBuf[ptr++] & 0xFF); // actual data bytes
+            }
+            assertEquals(0, outBuf[ptr++] & 0xFF);
+            for (int i = 0; i < 32; ++i) {
+                assertEquals(0xAA, outBuf[ptr++] & 0xFF); // actual data bytes
+            }
+        }
+    }
+
+    public void testLevel3Compressing()
+    {
+        final int BYTES = 32 * 16 * 8; // 4k
+        byte[] input;
+        BitmapEncoder encoder;
+        int result;
+
+        // with empty contents should just get straight run
+        input = new byte[BYTES];
+        encoder = new BitmapEncoder(input, input.length * 8);
+        assertEquals(0, encoder._encodeLevel3(0));
+        // but has now consumed input...
+        assertEquals(BYTES, encoder._inputPtr);
+        assertEquals(0, encoder._outputTail);
+        assertEquals(0, encoder._matchLevel1);
+
+        // with all 1s, bit different due to assumption of starting with '0'
+        input = new byte[BYTES];
+        Arrays.fill(input, (byte) 0xFF);
+        encoder = new BitmapEncoder(input, input.length * 8);
+        // 8-bit mask with first one set
+        assertEquals(0x80, encoder._encodeLevel3(0));
+        assertEquals(BYTES, encoder._inputPtr);
+        assertEquals(4, encoder._outputTail); // 1 actual byte at low level, 3 levels of masks
+        assertEquals(0x80, encoder._output[0] & 0xFF);
+        assertEquals(0x80, encoder._output[1] & 0xFF);
+        assertEquals(0x80, encoder._output[2] & 0xFF);
+        assertEquals(0xFF, encoder._output[3] & 0xFF);
+        assertEquals(0xFF, encoder._matchLevel1);
+
+        // and more, with half 0s, followed by half 1s
+        input = new byte[BYTES];
+        Arrays.fill(input, BYTES/2, input.length, (byte) 0xFF);
+        encoder = new BitmapEncoder(input, input.length * 8);
+        // should not be completely empty...
+        result = encoder._encodeLevel3(0);
+        assertEquals(BYTES, encoder._inputPtr);
+        assertEquals(0x08, result);
+        assertEquals(4, encoder._outputTail); // 1 data byte, plus 2 levels of prefixes
+        assertEquals(0x80, encoder._output[0] & 0xFF);
+        assertEquals(0x80, encoder._output[1] & 0xFF);
+        assertEquals(0x80, encoder._output[2] & 0xFF);
+        assertEquals(0xFF, encoder._output[3] & 0xFF);
+        assertEquals(0xFF, encoder._matchLevel1);
+    }
+    
+    public void testLevel3NotCompressing()
+    {
+        final int BYTES = 32 * 16 * 8; // 4k
+        byte[] input;
+        BitmapEncoder encoder;
+
+        // if non-compressing, should just get sets of literals
+        input = new byte[BYTES];
+        Arrays.fill(input, (byte)0xAA);
+        encoder = new BitmapEncoder(input, input.length * 8);
+        assertEquals(0xFF, encoder._encodeLevel3(0));
+        assertEquals(BYTES, encoder._inputPtr);
+        assertEquals(0, encoder._matchLevel1); // ends with 0-bit
+
+        assertEquals(BYTES, encoder._inputPtr);
+        // 1/32 for level 1 (optimized from 1/8); further 1/64 from level 2; and 1/512 for level 3
+        assertEquals(BYTES + BYTES/32 + BYTES/64 + BYTES/512, encoder._outputTail);
+
+        int ptr = 0;
+
+        byte[] outBuf = encoder._output;
+        for (int level3 = 0; level3 < 8; ++level3) {
+            assertEquals(0xFF, outBuf[ptr++] & 0xFF); // mask bits full at level3
+            for (int level2 = 0; level2 < 8; ++level2) { // 8 sections on level 2
+                assertEquals(0xFF, outBuf[ptr++] & 0xFF); // mask bits full at level2
+                assertEquals(0, outBuf[ptr++] & 0xFF); // short-cut marker to indicate "all 32"
+                for (int i = 0; i < 32; ++i) {
+                    assertEquals(0xAA, outBuf[ptr++] & 0xFF); // actual data bytes
+                }
+                assertEquals(0, outBuf[ptr++] & 0xFF);
+                for (int i = 0; i < 32; ++i) {
+                    assertEquals(0xAA, outBuf[ptr++] & 0xFF); // actual data bytes
+                }
+            }
+        }
     }
 }
